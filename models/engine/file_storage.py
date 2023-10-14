143d8fcdb709de0639820f3c09bfd1c2958576c8
tm-moumakoe <123243154+tm-moumakoe@users.noreply.github.com>
2023-10-14 06:43:00 -0700

    0x00. AirBnB clone - The console

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python3
 """Defines the FileStorage class"""
-from models.base_model import BaseModel
 import json
-import os
-
+from models.amenity import Amenity
+from models.base_model import BaseModel
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
 
 class FileStorage:
     """Represents a FileStorage"""
